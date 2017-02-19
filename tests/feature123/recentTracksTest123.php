b277cb9f65189f86eb5f0a3a12cb2c8cb5ef4c71
Barry van Veen <barryvanveen@gmail.com>
2017-02-19 14:36:30 +0100

    change casing of tests

--- tests/feature123/recentTracksTest123.php
+++ tests/feature123/recentTracksTest123.php
@@ -2,17 +2,17 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
 use Barryvanveen\Lastfm\Lastfm;
 use PHPUnit\Framework\TestCase;
 
-class topTracksTest extends TestCase
+class recentTracksTest extends TestCase
 {
     /** @test */
-    public function the_top_tracks_are_returned()
+    public function most_recent_tracks_are_returned()
     {
-        $tracks = Lastfm::userTopTracks('rj', $_ENV['LASTFM_API_KEY'])->get();
+        $tracks = Lastfm::userRecentTracks('rj', $_ENV['LASTFM_API_KEY'])->get();
 
         $this->assertNotEmpty($tracks);
         $this->assertCount(50, $tracks);
--- tests/feature123/topTracksTest123.php
+++ tests/feature123/topTracksTest123.php
@@ -2,17 +2,17 @@
 
 declare(strict_types=1);
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
 use Barryvanveen\Lastfm\Lastfm;
 use PHPUnit\Framework\TestCase;
 
-class recentTracksTest extends TestCase
+class topTracksTest extends TestCase
 {
     /** @test */
-    public function most_recent_tracks_are_returned()
+    public function the_top_tracks_are_returned()
     {
-        $tracks = Lastfm::userRecentTracks('rj', $_ENV['LASTFM_API_KEY'])->get();
+        $tracks = Lastfm::userTopTracks('rj', $_ENV['LASTFM_API_KEY'])->get();
 
         $this->assertNotEmpty($tracks);
         $this->assertCount(50, $tracks);
